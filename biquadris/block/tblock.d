e2df07303406cee76adad773892530b2bde9f9f6
Kai Lu <kaisiulu@gmail.com>
2020-04-12 21:50:56 -0400

    ptest

--- biquadris/block/tblock.d
+++ biquadris/block/tblock.d
@@ -1,4 +1,6 @@
 block/tblock.o: block/tblock.cc block/tblock.h block/../point/point.h \
- block/block.h block/../display/window.h block/../board/board.h \
- block/../board/cell.h block/../board/../subject/subject.h \
- block/../board/../point/point.h block/../board/../board/board.h
+  block/block.h block/../display/window.h /usr/X11/include/X11/Xlib.h \
+  /usr/X11/include/X11/X.h /usr/X11/include/X11/Xfuncproto.h \
+  /usr/X11/include/X11/Xosdefs.h block/../board/board.h \
+  block/../board/cell.h block/../board/../subject/subject.h \
+  block/../board/../point/point.h block/../board/../board/board.h
