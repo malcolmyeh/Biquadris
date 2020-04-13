c8ff3f1db18c4d8a8cd1d019b9b7b58f21839a9f
Kai Lu <kaisiulu@gmail.com>
2020-04-12 22:26:41 -0400

    fixed is placed

--- biquadris/display/window.d
+++ biquadris/display/window.d
@@ -1,1 +1,5 @@
-display/window.o: display/window.cc display/window.h
+display/window.o: display/window.cc /usr/X11/include/X11/Xlib.h \
+  /usr/X11/include/X11/X.h /usr/X11/include/X11/Xfuncproto.h \
+  /usr/X11/include/X11/Xosdefs.h /usr/X11/include/X11/Xutil.h \
+  /usr/X11/include/X11/keysym.h /usr/X11/include/X11/keysymdef.h \
+  display/window.h
--- biquadris/player/player.d
+++ biquadris/player/player.d
@@ -1,12 +1,14 @@
 player/player.o: player/player.cc player/player.h \
- player/../block/dblock.h player/../block/../point/point.h \
- player/../block/block.h player/../block/../display/window.h \
- player/../block/../board/board.h player/../block/../board/cell.h \
- player/../block/../board/../subject/subject.h \
- player/../block/../board/../point/point.h \
- player/../block/../board/../board/board.h player/../block/iblock.h \
- player/../block/jblock.h player/../block/lblock.h \
- player/../block/oblock.h player/../block/sblock.h \
- player/../block/tblock.h player/../block/zblock.h \
- player/../score/score.h player/../score/../subject/subject.h \
- player/../board/board.h player/../display/window.h
+  player/../block/dblock.h player/../block/../point/point.h \
+  player/../block/block.h player/../block/../display/window.h \
+  /usr/X11/include/X11/Xlib.h /usr/X11/include/X11/X.h \
+  /usr/X11/include/X11/Xfuncproto.h /usr/X11/include/X11/Xosdefs.h \
+  player/../block/../board/board.h player/../block/../board/cell.h \
+  player/../block/../board/../subject/subject.h \
+  player/../block/../board/../point/point.h \
+  player/../block/../board/../board/board.h player/../block/iblock.h \
+  player/../block/jblock.h player/../block/lblock.h \
+  player/../block/oblock.h player/../block/sblock.h \
+  player/../block/tblock.h player/../block/zblock.h \
+  player/../score/score.h player/../score/../subject/subject.h \
+  player/../board/board.h player/../display/window.h
