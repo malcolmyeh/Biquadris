8ef44e7dd6dd5c67284edc9a6af5564bab391bde
Kai Lu <kaisiulu@gmail.com>
2020-04-16 15:50:09 -0400

    dadas

---
+++ biquadris/board/nextblockboard.d
@@ -0,0 +1,10 @@
+board/nextblockboard.o: board/nextblockboard.cc board/nextblockboard.h \
+  board/board.h board/../point/point.h board/cell.h \
+  board/../subject/subject.h board/../board/board.h \
+  board/../display/window.h /usr/X11/include/X11/Xlib.h \
+  /usr/X11/include/X11/X.h /usr/X11/include/X11/Xfuncproto.h \
+  /usr/X11/include/X11/Xosdefs.h board/../block/block.h \
+  board/../block/../point/point.h board/../block/../display/window.h \
+  board/../block/../board/mainboard.h \
+  board/../block/../board/nextblockboard.h \
+  board/../block/../board/board.h
